ccfe4d1c3e2fb4bf099348f63e6e3fc764adf994
KipkoechMichael <90901415+KipkoechMichael@users.noreply.github.com>
2021-10-15 11:10:51 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+
+Project: County library management system
+Author: Kipkoech Michael Kiplangat
+Date: Oct,2021
+Compiler: GNU GCC
+Language: C99
+Licence: MIT
+
+*/
+int main()
+
+
+{
+    printf("Programming is fun! \n");
+    return 0;
+}
